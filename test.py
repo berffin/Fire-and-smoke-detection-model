05b818b148838226f8012265f3dbde677688e3e8
Berfin <122168480+berffin@users.noreply.github.com>
2026-08-18 00:53:38 +0300

    Update YOLO model weight file path

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 # Eğittiğiniz modelin ağırlık dosyasını (best.pt) yüklüyoruz
 # Klasör yolunu kendi bilgisayarınızdaki yere göre güncelleyebilirsiniz
-model = YOLO("runs\\detect\\Fire_Smoke_Detection\\yolo11_run_1-3\\weights\\best.pt")
+model = YOLO("best.pt")
 #Modeli bir video veya kamera üzerinde test etmek için predict fonksiyonunu kullanıyoruz.
 #Grafikten elde ettiğimiz ideal conf=0.39 ayarı tam olarak buraya yazılıyor!
 results = model.predict(source="g.mp4", show=True, conf=0.39)
